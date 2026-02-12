d42aef51cb1c33937467be4a1e1e86cc77df2aef
Margaret <240242@utxicotepec.edu.mx>
2026-02-12 11:49:13 -0600

    Agregacion de API

--- nombre-app/src/Promos.jsx
+++ nombre-app/src/Promos.jsx
@@ -1,9 +1,15 @@
+import Mapa from './Mapa'
 import './Promos.css'
 function Promos(){
     return (
         <div className="PromoContenedor">
             <h2>¡Hola bienvenido a nuestro sitio!</h2>
             <p>¡Ofertas en todos los productos!</p>
+            <Mapa
+                lat={19.4326}
+                lng={-99.1332}
+                nombre={"ServiTec Sucursal Centro"}
+            />
         </div>
     )
 }
