fa63c3b937810982ac0de907abe1f6fe835beb3b
Margaret <240242@utxicotepec.edu.mx>
2026-02-13 09:50:34 -0600

    Mapas

--- nombre-app/src/Promos.jsx
+++ nombre-app/src/Promos.jsx
@@ -1,15 +1,12 @@
-import Mapa from './Mapa'
+import MapaGeolocalizacion from './MapaGeolocalizacion'
 import './Promos.css'
 function Promos(){
     return (
         <div className="PromoContenedor">
             <h2>¡Hola bienvenido a nuestro sitio!</h2>
             <p>¡Ofertas en todos los productos!</p>
-            <Mapa
-                lat={19.4326}
-                lng={-99.1332}
-                nombre={"ServiTec Sucursal Centro"}
-            />
+            <MapaGeolocalizacion />
+         
         </div>
     )
 }
