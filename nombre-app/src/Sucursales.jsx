76bf021061659e74c5aabeb7d246be05f15c2c65
Margaret <240242@utxicotepec.edu.mx>
2026-02-13 00:33:16 -0600

    Sucursales

--- nombre-app/src/Sucursales.jsx
+++ nombre-app/src/Sucursales.jsx
@@ -1,21 +1,36 @@
+import Mapa from './Mapa';
 import './Sucursales.css'
-
-export default function Sucursales() {
-  return (
-    <div className="sucursalesSection">
-      <h2>Sucursales</h2>
-      <ul>
-        <li>Monterrey — Calle Reforma 123</li>
-        <li>Guadalajara — Av. Siempre Viva 456</li>
-        <li>Puebla — Centro 789</li>
-        <li>Sonora — Centro 1020</li>
-        <li>México — Av. Reforma 1000</li>
-        <li>Xicotepec — Calle Principal 500</li>
-        <li>Canadá — Av. Canadá 2000</li>
-        <li>Japón — Calle Principal 100</li>
-
-
-      </ul>
-    </div>
-  );
+function Sucursales() {
+    const sedes = [
+        { id: 1, ciudad: "Sonora", direccion: "Calle Reforma 123", tel: "+52 7822543107", mapa: "https://maps.google.com", lat: 19.438872567132496, lng: -99.15966103461886, nombre: "Sucursal SONORA" },
+        { id: 2, ciudad: "Chiapas", direccion: "Av. Siempre Viva 456", tel: "+52 7461301397", mapa: "https://maps.google.com", lat: 19.438872567132496, lng: -99.15966103461886, nombre: "Sucursal CHIAPAS" },
+        { id: 3, ciudad: "Durango", direccion: "Calle pricipal 500", tel: "+52 682346 5022", mapa: "https://maps.google.com", lat: 19.438872567132496, lng: -99.15966103461886, nombre: "Sucursal DURANGO" },
+        { id: 4, ciudad: "México", direccion: "Calle centro 202", tel: "+34 912 345 678", mapa: "https://maps.google.com", lat: 19.438872567132496, lng: -99.15966103461886, nombre: "Sucursal MÉXICO" }
+    ];
+    return (
+        <div className="classSuc">
+            <h2>Nuestras Sedes</h2>
+            <div className="classSedes">
+                {sedes.map((sede) => (
+                    <address key={sede.id}>
+                        <h3>{sede.ciudad}</h3>
+                        <p> {sede.direccion}</p>
+                        <p> <strong>{sede.tel}</strong></p>
+                        <Mapa
+                            lat={sede.lat}
+                            lng={sede.lng}
+                            nombre={sede.nombre} />
+                        <a
+                            href={sede.mapa}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Ver en Google Maps →
+                        </a>
+                    </address>
+                ))}
+            </div>
+        </div>
+    )
 }
+export default Sucursales
